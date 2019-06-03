f58d7760caaeb36c8719c0986aadb8d0e5eafd8b
cfowler650 <calebgithub@gmail.com>
2019-06-03 16:41:52 -0700

    Finished project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def index
+    user = User.all
+    render json: user  
+  end
+
+  def create
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else  
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+    def find_user_by_id
+      User.find(params[:id])
+    end
+
+    def show
+      render json: User.find(params[:id])
+    end
+
+    def update
+     @user = find_user_by_id
+     
+     @user.update!(user_params)
+     render json: @user
+    
+    end
+
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+
+    def destroy
+        @user = find_user_by_id
+
+        @user.destroy!
+        render json: @user
+    end
+end
